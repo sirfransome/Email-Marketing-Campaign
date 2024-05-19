fd04d6273448b5cac3ddbd2bf615e204931d55b0
Teniola Abiola <sirfransome@gmail.com>
2024-05-19 19:05:14 +0100

    included a predict button

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ preprocessing_pipeline = final_pipeline.named_steps['preprocessing']
 # Define the Streamlit app
 def main():
     st.title("Click-Through Rate Prediction App")
+    st.subheader("Unlock the potential of your email campaigns with AI-driven predictions!")
     numerical_features=['day_of_week','subject_len', 'body_len', 'mean_paragraph_len','no_of_CTA', 'mean_CTA_len', 'no_of_image','no_of_quotes','no_of_emoticons']
     categorical_features=['is_discount', 'is_price', 'is_urgency', 'is_personalised',  'is_weekend', 'sender', 'category', 'product', 'target_audience']
     one_hot_features=['times_of_day']
@@ -28,15 +29,16 @@ def main():
         # Preprocess data
         X = df_filtered  # Assuming the target column is not included in X
         st.write(X)
-        # X_transformed = final_pipeline.named_steps['preprocessing'].transform(X)
-        # Make predictions
-        predictions = final_pipeline.predict(X)
+        if st.button('Predict'):
+            # X_transformed = final_pipeline.named_steps['preprocessing'].transform(X)
+            # Make predictions
+            predictions = final_pipeline.predict(X)
 
-        # Add predictions to DataFrame
-        df_filtered['click_rate'] = predictions
+            # Add predictions to DataFrame
+            df_filtered['click_rate'] = predictions
 
-        # Display original dataset with predictions
-        st.write(df_filtered)
+            # Display original dataset with predictions
+            st.write(df_filtered)
 
 
 if __name__ == "__main__":
